637d877b1fab4f5fcd578d073621907774ff2123
raywaag <36991961+raywaag@users.noreply.github.com>
2018-12-06 10:37:24 -0500

    session change

--- session.php
+++ session.php
@@ -7,7 +7,7 @@
     $user_check = $_SESSION['loggedin'];
 
     // SQL Query To Fetch Complete Information Of User
-    $query = "SELECT username from people where username = '$user_check'";
+    $query = "SELECT username from actors where username = '$user_check'";
     $ses_sql = mysqli_query($db, $query);
     $row = mysqli_fetch_assoc($ses_sql);
     $login_session = $row['username'];
